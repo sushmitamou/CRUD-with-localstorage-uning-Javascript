c1b4ec42b591092dfa2d3cc5bbd33029bcad4788
sushmitamou <psushmita87@gmail.com>
2020-04-13 19:27:36 +0600

    filtering is added

--- script.js
+++ script.js
@@ -23,7 +23,62 @@ function loadEventListeners(){
     document.addEventListener('DOMContentLoaded', getTasks);
 }
 
-function addTask(tableIndex){                  
+function filterTask(){
+    $("#table").find("tr:not(:first)").remove();
+
+    var searchKeyword = $('#member_search').val();
+
+    var tasks;
+    if(localStorage.getItem('tasks') === null){
+        tasks = [] ;
+    }
+    else{
+        tasks = JSON.parse(localStorage.getItem('tasks'));  
+    }
+
+    var filteredMembers = tasks.filter(function (item, index){
+    return  item.firstname.toLowerCase().includes(searchKeyword.toLowerCase()) ||
+            item.lastname.toLowerCase().includes(searchKeyword.toLowerCase()) ||
+            item.email.toLowerCase().includes(searchKeyword.toLowerCase()) ||
+            item.designation.toLowerCase().includes(searchKeyword.toLowerCase())
+    });
+
+    if (!filteredMembers.length) {
+        $('.show-table-info').removeClass('hide');
+    } else {
+        $('.show-table-info').addClass('hide');
+    }
+    console.log(filteredMembers);
+    
+
+
+    filteredMembers.forEach(function (item, index) {
+
+        newRow= table.insertRow(table.length);
+        cell1 = newRow.insertCell(0);
+        cell2 = newRow.insertCell(1);
+        cell3 = newRow.insertCell(2);
+        cell4 = newRow.insertCell(3);
+        cell5 = newRow.insertCell(4);
+        cell6 = newRow.insertCell(5);
+        cell7 = newRow.insertCell(6);
+
+
+        cell1.innerHTML = index+1;
+        cell2.innerHTML = item.firstname;
+        cell3.innerHTML = item.lastname;
+        cell4.innerHTML = item.email;
+        cell5.innerHTML = item.age;
+        cell6.innerHTML = item.designation; 
+        var guid = id;
+        cell7.innerHTML ='<button class="btn btn-sm btn-default" onclick="viewModal('+ guid +');">View</button> ' +
+                '<button class="btn btn-sm btn-primary" onclick="editModal(' + guid + ')">Edit</button> ' +
+                '<button class="btn btn-sm btn-danger" onclick="showDeleteModal(' + guid + ')">Delete</button>';
+            
+    })
+}
+
+function addTask(){                  
   var table   = document.querySelector('table tbody');
   newRow      = table.insertRow(table.length);
 
@@ -37,7 +92,7 @@ function addTask(tableIndex){
   cell7 = newRow.insertCell(6);
 
 
-  cell1.innerHTML = tableIndex;
+  cell1.innerHTML = getTotalRowOfTable();
   cell2.innerHTML = firstname.value;
   cell3.innerHTML = lastname.value;
   cell4.innerHTML = email.value;
@@ -49,7 +104,7 @@ function addTask(tableIndex){
         '<button class="btn btn-sm btn-primary" onclick="editModal(' + guid + ')">Edit</button> ' +
         '<button class="btn btn-sm btn-danger" onclick="showDeleteModal(' + guid + ')">Delete</button>';
    
-   var obj = {    
+   var obj = {   
         firstname  : firstname.value,
         lastname   : lastname.value,
         email      : email.value,
@@ -57,17 +112,17 @@ function addTask(tableIndex){
         designation: designation.value,       
     };
     
-    console.log(obj);
+    //console.log(obj);
     
     addDataInLocalStorage(obj);
     clearForm();
-
+    $('#member-form-modal').modal('hide')
 }
 
 //counting row of table
 function getTotalRowOfTable() {
     var table = document.getElementById('table');
-    return table.rows.length;
+    return table.rows.length-1;
 }
 
 //store data in local storage
@@ -112,7 +167,7 @@ function getTasks(){
         cell6 = newRow.insertCell(5);
         cell7 = newRow.insertCell(6);
 
-        cell1.innerHTML = index + 1;
+        cell1.innerHTML = getTotalRowOfTable();
         cell2.innerHTML = obj.firstname;
         cell3.innerHTML = obj.lastname;
         cell4.innerHTML = obj.email;
@@ -121,8 +176,8 @@ function getTasks(){
         var guid = obj.id;  
         cell7.innerHTML = '<button class="btn btn-sm btn-default" onclick="viewModal('+ guid +');">View</button>' +
         '<button class="btn btn-sm btn-primary" onclick="editModal('+ guid +')" >Edit</button> ' +
-        '<button class="btn btn-sm btn-danger" onclick="showDeleteModal(' + guid + ')">Delete</button>';
-        
+        '<button class="btn btn-sm btn-danger" onclick="showDeleteModal(' + guid + ')">Delete</button>';   
+    
     });
 }
 
@@ -151,52 +206,6 @@ function deleteMemberData() {
     getTasks();
 }
 
-// function filterTask(){
-
-//     $("#table").find("tr:not(:first)").remove();
-
-//     var searchKeyword = $('#member_search').val();
-//    // var members = getFormattedMembers();
-
-//    var data  = localStorage.getItem('tasks');
-//    var tasks = JSON.parse(data);
-
-
-//     var filteredMembers = tasks.filter(function (item, index) {
-//         return item.firstname.toLowerCase().includes(searchKeyword.toLowerCase()) ||
-//             item.lastname.toLowerCase().includes(searchKeyword.toLowerCase()) ||
-//             item.email.toLowerCase().includes(searchKeyword.toLowerCase()) ||
-//             item.designation.toLowerCase().includes(searchKeyword.toLowerCase())
-//     });
-
-//     if (!filteredMembers.length) {
-//         $('.show-table-info').removeClass('hide');
-//     } else {
-//         $('.show-table-info').addClass('hide');
-//     }
-
-//     filteredMembers.forEach(function (item, index) {
-//          addTask(item, index + 1);
-       
-//      })
-    
-// }
-
-
-// function filterTask(e){
-//     // const text = e.target.value.toLowerCase();
-
-//     //  document.querySelector('tasks').forEach(function(task){
-//     //      const item = task.firstChild.textContent;
-
-//     //      if(item.toLowerCase().indexOf(text) != -1){
-//     //          task.style.display = 'block';
-//     //      }
-//     //      else{
-//     //          task.style.display = 'none';
-//     //      }
-//     // });
-// }
 
 //clear form data
 function clearForm(){
